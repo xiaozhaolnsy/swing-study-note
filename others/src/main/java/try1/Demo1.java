f7ae393c3829b24cc167ff25ef7a527365f9bcd7
xiaozhaolnsy <xiaozhaolnsy@126.com>
2020-07-24 15:19:45 +0800

    project init

---
+++ others/src/main/java/try1/Demo1.java
@@ -0,0 +1,28 @@
+package try1;
+
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTree;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.JTabbedPane;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollBar;
+
+public class Demo1 {
+    private JButton button1;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JFormattedTextField formattedTextField1;
+    private JTree tree1;
+    private JList list1;
+    private JTable table1;
+    private JTabbedPane tabbedPane1;
+    private JProgressBar progressBar1;
+    private JScrollBar scrollBar1;
+}
